cfb44d70e5ba768d685a4ae8cbae521cca55580d
Cristina <kristikavai@mail.ru>
2023-08-07 10:49:52 +0300

    change str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def string_func(word):
+    """Возвращает слово заглавными буквами"""
     return word.upper()
+
+
+def cap_string_func(word):
+    """Возвращает слово с заглавной буквы"""
+    return word.capitalize()
